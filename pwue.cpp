1ee407351fba1deb269adf117f2bf1925232d614
Finn Rudolph <finn.rudolph@t-online.de>
2023-04-28 17:48:23 +0200

    Add program for P(18)

--- pwue.cpp
+++ pwue.cpp
@@ -3,12 +3,13 @@
 #include <cstdint>
 #include <future>
 #include <iostream>
+#include <shared_mutex>
 #include <thread>
 #include <vector>
 
 #include <immintrin.h>
 
-constexpr size_t N = 15;
+constexpr size_t N = 18;
 
 using vec64_4 = uint64_t __attribute__((vector_size(32)));
 
@@ -160,147 +161,150 @@ void dp(size_t n, uint8_t const *const y, uint8_t *const z, uint64_t i1, uint64_
     }
 }
 
-// Gehe 1 Ebene herunter.
-std::pair<uint64_t, uint64_t> bf1(
-    size_t n, uint8_t const *const y, size_t i1, size_t i2)
+std::pair<uint64_t, uint64_t> bf5(
+    uint8_t const *const y, size_t i1, size_t i2, std::shared_mutex *mut)
 {
-    uint64_t const u = factorial(n), v = factorial(n - 1);
+    uint64_t const u = factorial(18), v = factorial(13);
     std::pair<uint64_t, uint64_t> result = {0, 0};
-    vec64_4 p[N];
-    vec_ith_permutation(n, i1, p, 0);
-    vec_generate_permutations(n, p);
+    vec64_4 p[N], q1[N], q2[N], q3[N], q4[N];
+    vec_ith_permutation(18, i1, p, 0);
+    vec_generate_permutations(18, p);
 
     for (uint64_t i = i1; i < i2; i += 4)
     {
-        vec64_4 a1 = {n, n, n, n}, a2 = {n, n, n, n};
+        vec64_4 a1 = {18, 18, 18, 18}, a2 = {18, 18, 18, 18};
 
-        for (size_t j = 0; j < n; ++j)
+        for (size_t j1 = 0; j1 < 18; ++j1)
         {
-            vec64_4 const l = linear_mu_gamma(n, p, j);
-
-            a1[0] = std::min<uint64_t>(a1[0], y[l[0]] + 1);
-            a1[1] = std::min<uint64_t>(a1[1], y[l[1]] + 1);
-            a1[2] = std::min<uint64_t>(a1[2], y[l[2]] + 1);
-            a1[3] = std::min<uint64_t>(a1[3], y[l[3]] + 1);
-
-            a2[0] = std::min<uint64_t>(a2[0], y[v - l[0] - 1] + 1);
-            a2[1] = std::min<uint64_t>(a2[1], y[v - l[1] - 1] + 1);
-            a2[2] = std::min<uint64_t>(a2[2], y[v - l[2] - 1] + 1);
-            a2[3] = std::min<uint64_t>(a2[3], y[v - l[3] - 1] + 1);
-        }
-
-        for (size_t j = 0; j < 4; ++j)
-        {
-            if (a1[j] > result.first)
-                result = {a1[j], i + j};
-            if (a2[j] > result.first)
-                result = {a2[j], u - i - 1 - j};
-        }
-
-        for (size_t i = 0; i < n; ++i)
-            p[i][0] = p[i][3];
-        vec_next_permutation(n, p, 0);
-        vec_generate_permutations(n, p);
-    }
-
-    return result;
-}
-
-// Gehe 2 Ebenen herunter.
-std::pair<uint64_t, uint64_t> bf2(
-    size_t n, uint8_t const *const y, size_t i1, size_t i2)
-{
-    uint64_t const u = factorial(n), v = factorial(n - 2);
-    std::pair<uint64_t, uint64_t> result = {0, 0};
-    vec64_4 p[N], q[N];
-    vec_ith_permutation(n, i1, p, 0);
-    vec_generate_permutations(n, p);
-
-    for (uint64_t i = i1; i < i2; i += 4)
-    {
-        vec64_4 a1 = {n, n, n, n}, a2 = {n, n, n, n};
-
-        for (size_t j = 0; j < n; ++j)
-        {
-            gamma(n, p, q, j);
-            for (size_t k = 0; k < n - 1; ++k)
+            gamma(18, p, q1, j1);
+            for (size_t j2 = 0; j2 < 17; ++j2)
             {
-                vec64_4 const l = linear_mu_gamma(n - 1, q, k);
+                gamma(17, q1, q2, j2);
+                for (size_t j3 = 0; j3 < 16; ++j3)
+                {
+                    gamma(16, q2, q3, j3);
+                    for (size_t j4 = 0; j4 < 15; ++j4)
+                    {
+                        gamma(15, q3, q4, j4);
+                        for (size_t j5 = 0; j5 < 14; ++j5)
+                        {
+                            vec64_4 const l = linear_mu_gamma(14, q4, j5);
 
-                a1[0] = std::min<uint64_t>(a1[0], y[l[0]] + 2);
-                a1[1] = std::min<uint64_t>(a1[1], y[l[1]] + 2);
-                a1[2] = std::min<uint64_t>(a1[2], y[l[2]] + 2);
-                a1[3] = std::min<uint64_t>(a1[3], y[l[3]] + 2);
+                            a1[0] = std::min<uint64_t>(a1[0], y[l[0]] + 2);
+                            a1[1] = std::min<uint64_t>(a1[1], y[l[1]] + 2);
+                            a1[2] = std::min<uint64_t>(a1[2], y[l[2]] + 2);
+                            a1[3] = std::min<uint64_t>(a1[3], y[l[3]] + 2);
 
-                a2[0] = std::min<uint64_t>(a2[0], y[v - l[0] - 1] + 2);
-                a2[1] = std::min<uint64_t>(a2[1], y[v - l[1] - 1] + 2);
-                a2[2] = std::min<uint64_t>(a2[2], y[v - l[2] - 1] + 2);
-                a2[3] = std::min<uint64_t>(a2[3], y[v - l[3] - 1] + 2);
+                            a2[0] = std::min<uint64_t>(a2[0], y[v - l[0] - 1] + 2);
+                            a2[1] = std::min<uint64_t>(a2[1], y[v - l[1] - 1] + 2);
+                            a2[2] = std::min<uint64_t>(a2[2], y[v - l[2] - 1] + 2);
+                            a2[3] = std::min<uint64_t>(a2[3], y[v - l[3] - 1] + 2);
+
+                            if (a1[0] < 11 && a1[1] < 11 && a1[2] < 11 && a1[3] < 11 &&
+                                a2[0] < 11 && a2[1] < 11 && a2[2] < 11 && a2[3] < 11)
+                                goto next_permutation_block;
+                        }
+                    }
+                }
             }
         }
 
         for (size_t j = 0; j < 4; ++j)
         {
+            if (a1[j] == 11)
+            {
+                std::unique_lock lck(*mut);
+                for (size_t i = 0; i < 18; ++i)
+                    std::cout << p[i][j] + 1 << ' ';
+                std::cout << std::endl;
+            }
+            if (a2[j] == 11)
+            {
+                vec_ith_permutation(18, u - i - 1 - j, q1, 0);
+                std::unique_lock lck(*mut);
+                for (size_t i = 0; i < 18; ++i)
+                    std::cout << q1[i][0] + 1 << ' ';
+                std::cout << std::endl;
+            }
             if (a1[j] > result.first)
                 result = {a1[j], i + j};
             if (a2[j] > result.first)
                 result = {a2[j], u - i - 1 - j};
         }
 
-        for (size_t i = 0; i < n; ++i)
+    next_permutation_block:
+        for (size_t i = 0; i < 18; ++i)
             p[i][0] = p[i][3];
-        vec_next_permutation(n, p, 0);
-        vec_generate_permutations(n, p);
+        vec_next_permutation(18, p, 0);
+        vec_generate_permutations(18, p);
     }
 
     return result;
+}
+
+std::pair<uint64_t, uint64_t> get_mu_interval(size_t n)
+{
+    std::cout << "Intervall an zu berechnenden Permutationen einschränken? [y/n] ";
+    char c;
+    std::cin >> c;
+    if (c == 'n')
+        return {0, factorial(n) / 2};
+    uint64_t mu_begin, mu_end;
+    std::cout << "Das Intervall muss Teil von [0, n! / 2) = [0, " << factorial(n) / 2 << ") sein.\n"
+              << "Die Länge des Intervalls muss ein Vielfaches von "
+              << 8 * std::thread::hardware_concurrency() << " sein.\n"
+              << "Anfang (inklusiv): ";
+    std::cin >> mu_begin;
+    std::cout << "Ende (exklusiv): ";
+    std::cin >> mu_end;
+    return {mu_begin, mu_end};
 }
 
 int main()
 {
     size_t n;
     std::cin >> n;
-    if (n < 6)
+    if (n < 4)
     {
-        std::cout << "Funktioniert nur für n >= 6...\n";
+        std::cout << "Funktioniert nur für n >= 4...\n";
         return 0;
     }
 
-    uint8_t *a = (uint8_t *)malloc(factorial(12)),
+    auto const [mu_begin, mu_end] = get_mu_interval(n);
+    auto const start_time = std::chrono::system_clock::now();
+
+    uint8_t *a = (uint8_t *)malloc(factorial(13)),
             *b = (uint8_t *)malloc(factorial(12));
     a[0] = 0, a[1] = 1, a[2] = 1, a[3] = 2, a[4] = 1, a[5] = 1;
     size_t const num_threads = std::thread::hardware_concurrency();
 
-    for (size_t k = 4; k <= std::min<size_t>(n - 1, 13); ++k)
+    for (size_t k = 4; k <= 13; ++k)
     {
         std::vector<std::thread> threads;
         if (!(factorial(k) & 15))
         {
             for (size_t t = 0; t < num_threads; t++)
-                threads.emplace_back(dp, k, (uint8_t *)((k & 1) ? b : a),
-                                     (uint8_t *)((k & 1) ? a : b),
+                threads.emplace_back(dp, k, a, b,
                                      (factorial(k) / 2) * t / num_threads,
                                      (factorial(k) / 2) * (t + 1) / num_threads);
         }
         else
         {
-            threads.emplace_back(dp, k, (uint8_t *)((k & 1) ? b : a),
-                                 (uint8_t *)((k & 1) ? a : b),
-                                 0, factorial(k) / 2);
+            threads.emplace_back(dp, k, a, b, 0, factorial(k) / 2);
         }
         for (std::thread &t : threads)
             t.join();
-        ((k & 1) ? a : b)[0] = 0;
+        std::swap(a, b);
     }
 
     std::pair<uint64_t, uint64_t> result = {0, 0};
     std::vector<std::future<std::pair<uint64_t, uint64_t>>> fut;
+    std::shared_mutex mut;
 
     for (size_t t = 0; t < num_threads; ++t)
-        fut.emplace_back(async(n <= 14 ? bf1 : bf2, n,
-                               (uint8_t *)((n & 1) && n != 15 ? b : a),
-                               (factorial(n) / 2) * t / num_threads,
-                               (factorial(n) / 2) * (t + 1) / num_threads));
+        fut.emplace_back(async(bf5, a,
+                               mu_begin + (mu_end - mu_begin) * t / num_threads,
+                               mu_begin + (mu_end - mu_begin) * (t + 1) / num_threads, &mut));
 
     for (auto &f : fut) // Finde das maximale A(p) aller Threads.
         result = max(result, f.get());
@@ -309,7 +313,9 @@ int main()
     free(b);
     vec64_4 p[N];
     vec_ith_permutation(n, result.second, p, 0);
-    std::cout << "P(" << n << ") = " << result.first
+    std::cout << "Laufzeit: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::system_clock::now() - start_time)).count()
+              << "P(" << n << ") = " << result.first
               << ", Beispiel mit A(p) = P(" << n << ") : ";
     for (size_t i = 0; i < n; ++i)
         std::cout << p[i][0] + 1 << ' ';
